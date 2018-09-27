161c47399cd2765a425cfb0d78aadce3a2e62842
Luiz Otavio Vidoto <luizvidoto@NIC-PRO.local>
2018-09-27 20:02:40 -0300

    Added react-native-fs pkg

--- android/app/src/main/java/com/rnnapp/MainApplication.java
+++ android/app/src/main/java/com/rnnapp/MainApplication.java
@@ -13,6 +13,7 @@ import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 
+import com.rnfs.RNFSPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,7 +41,7 @@ public class MainApplication extends NavigationApplication {
         // Add additional packages you require here
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
-                // eg. new VectorIconsPackage()
+                new RNFSPackage()
         );
     }
 
